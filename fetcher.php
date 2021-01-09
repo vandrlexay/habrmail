a5dc5fc0ed8b7d0054c885159270bd4753467886
zapad <zargener@gmail.com>
2021-01-09 04:19:55 +0300

    Clearance.

--- fetcher.php
+++ fetcher.php
@@ -7,16 +7,12 @@ require_once "lib/autoload.php";
 use ItIsAllMail\DriverFactory;
 use ItIsAllMail\Mailbox;
 
-
 $config = yaml_parse_file(__DIR__ . DIRECTORY_SEPARATOR . "conf" . DIRECTORY_SEPARATOR . "config.yml");
 $sources = yaml_parse_file(__DIR__ . DIRECTORY_SEPARATOR . "conf" . DIRECTORY_SEPARATOR . "sources.yml");
 
 $driverFactory = new DriverFactory($config);
 
 foreach ($sources as $source) {
-
-    $driver = null;
-    
     $driver = $driverFactory->getDriverForSource($source);
 
     $posts = $driver->getPosts($source);
